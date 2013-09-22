e94f18aaeb30f917cc399b2284fe062ba4672423
moormanm <michael.e.moorman@gmail.com>
2013-09-22 09:38:05 -0400

    first commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.shopcierge.com/admin/');
+define('HTTP_CATALOG', 'http://www.shopcierge.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.shopcierge.com/admin/');
+define('HTTPS_CATALOG', 'http://www.shopcierge.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/opencart/admin/');
+define('DIR_SYSTEM', '/var/www/html/opencart/system/');
+define('DIR_DATABASE', '/var/www/html/opencart/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/opencart/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/opencart/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/html/opencart/image/');
+define('DIR_CACHE', '/var/www/html/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/opencart/download/');
+define('DIR_LOGS', '/var/www/html/opencart/system/logs/');
+define('DIR_CATALOG', '/var/www/html/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'shopcierge0');
+define('DB_PASSWORD', 'assd22');
+define('DB_DATABASE', 'shopcierge0');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.shopcierge.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.shopcierge.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/opencart/catalog/');
+define('DIR_SYSTEM', '/var/www/html/opencart/system/');
+define('DIR_DATABASE', '/var/www/html/opencart/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/html/opencart/image/');
+define('DIR_CACHE', '/var/www/html/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/opencart/download/');
+define('DIR_LOGS', '/var/www/html/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'shopcierge0');
+define('DB_PASSWORD', 'assd22');
+define('DB_DATABASE', 'shopcierge0');
+define('DB_PREFIX', 'oc_');
+?>
